f883a538444d07a9bd529ecc41172568d4b6c8f3
CarlosPhilipe <carlosphbahia@gmail.com>
2019-09-03 00:10:48 -0400

    WIP

--- handler.js
+++ handler.js
@@ -1,45 +1,75 @@
 'use strict';
+
 const AWS = require('aws-sdk');
 var s3 = new AWS.S3();
+const fileType = require('file-type');
 
-module.exports.hello = async event => {
-  // restore image sent by request
-  let encodedImage = JSON.parse(event.body).user_avatar;
-  // trnsated image to b64 fom data
-  let decodedImage = Buffer.from(encodedImage, 'base64');
+const getFile = (fileMime, buffer) => {
+  let fileExt = fileMime.ext;
+  let hash = sha1(new Buffer(new Date().toString()));
+  let now = moment().format('yyyy-MM-DD HH:mm:ss');
 
-  var filePath = "avatars/name.jpg";
+  let filePath = hash + '/';
+  let fileName = unixTime(now) + '.' + fileExt;
+  let fileFullName = filePath + fileName;
+  let fileFullPath = 'static.lili.com.br/' + fileFullName;
 
-  var params = {
-      Body: decodedImage,
-      Bucket: "static.lili.com.br",
-      Key: filePath,
-   };
+  let params = {
+    Bucket: "static.lili.com.br",
+    Key: fileFullName,
+    Body: buffer,
+  };
 
-   s3.upload(params, function(err, data) {
-       if(err) {
-           return {
-             statusCode: 400,
-             body: JSON.stringify(
-               {
-                 message: 'error!',
-                 err,
-               },
-             ),
-           };
-       } else {
-         return {
-           statusCode: 200,
-           body: JSON.stringify(
-             {
-               message: 'successfully!',
-             },
-           ),
-         };
+  let uploadFile = {
+    size: buffer.toString('ascii').length,
+    type: fileMime.mime,
+    name: fileName,
+    full_path: fileFullPath,
+  }
+
+  return {
+    params: params,
+    uploadFile: uploadFile,
+  };
+}
+
+module.exports.hello = async (event, context) => {
+  try {
+    let request = event.body;
+    // restore image sent by request
+    let encodedImage = JSON.parse(request).user_avatar;
+    // trnsated image to b64 fom data
+    let decodedImage = Buffer.from(encodedImage, 'base64');
+
+    let fileMime = fileType(decodedImage);
+
+    let file = getFile(fileMime, decodedImage);
+
+    let params = file.params;
+
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify(
+            {
+              message: 'deu ruim!',
+              JSON.stringify(err),
+            },
+          ),
+        };
       }
-  });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: 'successfully!',
+          },
+        ),
+      };
+    });
 
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  } catch (err) {
+    return JSON.stringify(err);
+  }
 };
